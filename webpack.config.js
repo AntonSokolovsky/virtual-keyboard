4be2d998dee86846033dda8505a503ac3c1c9f14
AntonSokolovsky <anton.a.sokolovsky@gmail.com>
2023-04-30 20:50:56 +0300

    feat: add dependencies

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  module: {
+    rules: [
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        }
+      ], 
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+         title: "Virtual Keyboard"
+        }),
+        new MiniCssExtractPlugin({
+        filename: 'styles.css',
+        })
+    ]
+};
